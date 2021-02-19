1f6815ce329654f34d1374d2f2a0709739fb9e20
borisRadojicic1 <boris.radojicic1@gmail.com>
2021-02-19 12:05:31 +0100

    docker-compose config setup, start scripts added

---
+++ clean_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./mvnw clean package -DskipTests
+cp target/PollsService-0.0.1-SNAPSHOT.jar src/main/docker
+
+cd src/main/docker || exit
+sudo docker-compose down
+sudo docker rmi docker-spring-boot-postgres:latest
+sudo docker-compose up
---
+++ restart.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd src/main/docker || exit
+sudo docker-compose up
